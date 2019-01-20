dc9ba83d930a9d951a5961e56231b19115694b43
bandyopadhyay <amlanima.gmail.com>
2019-01-19 19:26:25 -0600

    problem 46

---
+++ Backtracking/src/Permutations/Solution.java
@@ -0,0 +1,48 @@
+package Permutations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * https://leetcode.com/problems/permutations/
+ * 46. Permutations
+ * Given a collection of distinct integers, return all possible permutations.
+ */
+public class Solution {
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> temp = new ArrayList<Integer>();
+		backtrack(result, temp, nums);
+		return result;
+	}
+
+	private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums) {
+		if (temp.size() == nums.length) {
+			result.add(new ArrayList<Integer>(temp));
+		} else {
+			for (int i = 0; i < nums.length; i++) {
+				if (temp.contains(nums[i]))
+					continue;
+				temp.add(nums[i]); // choose
+				backtrack(result, temp, nums);
+				temp.remove(temp.size() - 1); // not choose
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int arr[] = { 1, 2, 3, 4 };
+		print(s.permute(arr));
+	}
+
+	private static void print(List<List<Integer>> result) {
+		for (List<Integer> a : result) {
+			for (Integer b : a) {
+				System.out.print(b + ", ");
+			}
+			System.out.println();
+		}
+
+	}
+}
